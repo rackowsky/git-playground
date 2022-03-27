da68b05d380c14635ee1d25c275bfd25b91905a9
rackowsky <bartek@wroconsult.pl>
2022-03-27 20:37:19 +0200

    new style classes

--- release-1.6.js
+++ release-1.6.js
@@ -3,6 +3,28 @@ import styled from "styled-components"
 export const Test = styled.div`
 
 `
+export const sdfsdf = styled.h1`
+    
+`
+export const sdsdf = styled.h6`
+    
+`
+export const Tesdfsdfsfsfst = styled.div`
+
+`
+export const sdfsdfsdf = styled.h1`
+    
+`
+export const Test3 = styled.h6`
+    
+`
+
+export const sdfsdfsdfsfsf = styled.div`
+
+`
 export const Test2 = styled.h1`
+    
+`
+export const sdfsdfs = styled.h6`
     
 `
