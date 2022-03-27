56d714c7663a711102dcadea631697d600e5a6e2
rackowsky <bartek@wroconsult.pl>
2022-03-27 20:43:28 +0200

    new page was added

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const IndexPage = () => {
+    return (
+        <h1
+            name={test.test2}
+            category="files"
+        >Test</h1>
+    )
+}
+
+export default IndexPage
